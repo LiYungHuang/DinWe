d7c1151efab4ff0cf6d97e3c434c2873d69e6c7f
黃莉詠 <polily20331@gmail.com>
2023-05-22 18:02:40 +0800

    新增 "/" 首頁路徑
    (cherry picked from commit 164f384d7e2369986c114f340b288489ad5b0474)

--- React/src/App.js
+++ React/src/App.js
@@ -1,7 +1,20 @@
-import React, { Component } from "react";
+import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/js/bootstrap.js";
+
+// index
+import "./components/css/index.css";
+import Index from "./components/index/Index";
+
+//Enterprise
+import Enterprise from "./components/enterprise/Enterprise";
+
+//member
+import MemberMain from "./components/member/MemberMain";
+import "./components/css/member.css";
+
+//Search
 import SearchIndex from "./components/search/SearchIndex";
 import Page2 from "./components/index/test";
 
@@ -15,21 +28,18 @@ import Login from "./components/admin/login_Roy";
 import Register from "./components/admin/register_Roy";
 import ResetStore from "./components/admin/resetStore";
 
-//member
-import MemberMain from "./components/member/MemberMain";
-import "./components/css/member.css";
-
-// index
-import "./components/css/index.css";
-import Index from "./components/index/Index";
-
-//Enterprise
-import Enterprise from "./components/enterprise/Enterprise";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Switch>
+        {/* index */}
+        <Route path="/" component={Index} exact/>
+        <Route path="/index" component={Index} />
+        {/* Enterprise */}
+        <Route path="/enterprise" component={Enterprise} />
+        {/* member */}
+        <Route path="/membermain" component={MemberMain} />
         {/* Search */}
         <Route path="/test" component={Page2}></Route>
         <Route path="/searchIndex" component={SearchIndex}></Route>
@@ -42,12 +52,6 @@ const App = () => {
         <Route path="/resetPwd/:token/:id" component={ResetPwd} />
         <Route path="/resetStore/:token/:id" component={ResetStore} />
         <Route path="/openStore/:token/:account" component={OpenStore} />
-        {/* index */}
-        <Route path="/index" component={Index} />
-        {/* Enterprise */}
-        <Route path="/enterprise" component={Enterprise} />
-        {/* member */}
-        <Route path="/membermain" component={MemberMain} />
       </Switch>
     </BrowserRouter>
   );
